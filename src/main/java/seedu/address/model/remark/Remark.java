7cd2f87ccc4ac0972f60162d522a4fd94d1d2346
appleraincoat <james20111951@gmail.com>
2024-03-15 22:57:00 +0800

    Remove constraints for the remark

--- src/main/java/seedu/address/model/remark/Remark.java
+++ src/main/java/seedu/address/model/remark/Remark.java
@@ -9,8 +9,8 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
  */
 public class Remark {
 
-    public static final String MESSAGE_CONSTRAINTS = "Remark names should be alphanumeric";
-    public static final String VALIDATION_REGEX = "\\p{Alnum}+";
+    public static final String MESSAGE_CONSTRAINTS = "Remark names can contain any character.";
+    public static final String VALIDATION_REGEX = ".+";
 
     public final String remarkName;
 
